b3c33880d882c61a3b19ec57c09a235928843f85
captainCwangrz <18501397628@163.com>
2025-09-03 22:48:06 +0800

    added tilemap tests. added self.assets.

--- game.py
+++ game.py
@@ -1,24 +1,32 @@
 import pygame, sys
 from scripts.entities import PhysicsEntity
-from scripts.utils import load_image
+from scripts.utils import load_image, load_images
+from scripts.tilemap import Tilemap
 
 class Game:
   def __init__(self):
     pygame.init()
     self.screen = pygame.display.set_mode((640, 480))
+    self.display = pygame.Surface((320, 240)) # To be stretched
     pygame.display.set_caption("Ninja Game")
     self.clock = pygame.time.Clock()
 
     self.assets = {
-      'Player' : load_image('entities/player.png')
+      'Player' : load_image('entities/player.png'),
+      'Decor' : load_images('tiles/decor'),
+      'Grass' : load_images('tiles/grass'),
+      'Large Decor' : load_images('tiles/large_decor'),
+      'Stone' : load_images('tiles/stone')
     }
     self.player = PhysicsEntity(self, 'Player', (50, 50), (8, 15))
     self.movement = [False, False] #up, down
 
+    self.tilemap = Tilemap(self)
+
   def run(self):
     while True:
       self.clock.tick(60)
-      self.screen.fill((14, 219, 228))
+      self.display.fill((14, 219, 228))
 
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -36,8 +44,9 @@ class Game:
             self.movement[1] = False
 
       self.player.update((self.movement[1] - self.movement[0], 0))
-      self.player.render(self.screen)
-
+      self.player.render(self.display)
+      self.tilemap.render(self.display)
+      self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0,0))
       pygame.display.flip() #safe full-page refresh, otherwise use update([rect_list])
 
 Game().run()
