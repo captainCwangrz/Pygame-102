1a81e47da0e9bdef9ecc2debf7a312a625b782e6
captainCwangrz <18501397628@163.com>
2025-09-04 00:29:06 +0800

    added collision detection and rect(). handles gravity reset on y axis

--- scripts/entities.py
+++ scripts/entities.py
@@ -7,11 +7,41 @@ class PhysicsEntity:
     self.pos = list(pos) #prevent global edit and handles collections
     self.size = size
     self.velocity = [0, 0]
+    self.collisions = {'UP' : False, 'DOWN' : False, 'LEFT' : False, 'RIGHT' : False}
   
-  def update(self, movement=(0,0)):
+  def rect(self):
+    return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+
+  def update(self, tilemap, movement=(0,0)):
+    self.collisions = {'UP' : False, 'DOWN' : False, 'LEFT' : False, 'RIGHT' : False}
+
     frame_movement = (movement[0]+self.velocity[0], movement[1]+self.velocity[1])
     self.pos[0] += frame_movement[0]
+    entity_rect = self.rect()
+    for rect in tilemap.physics_tiles_around(self.pos):
+      if rect.colliderect(entity_rect):
+        if frame_movement[0] > 0:
+          self.collisions['RIGHT'] = True
+          entity_rect.right = rect.left
+        if frame_movement[0] < 0:
+          self.collisions['LEFT'] = True
+          entity_rect.left = rect.right
+        self.pos[0] = entity_rect.x
+    
     self.pos[1] += frame_movement[1]
+    entity_rect = self.rect()
+    for rect in tilemap.physics_tiles_around(self.pos):
+      if rect.colliderect(entity_rect):
+        if frame_movement[1] > 0:
+          self.collisions['DOWN'] = True
+          entity_rect.bottom = rect.top
+        if frame_movement[1] < 0:
+          self.collisions['UP'] = True
+          entity_rect.top = rect.bottom
+        self.pos[1] = entity_rect.y
+    
+    if self.collisions['UP'] or self.collisions['DOWN']:
+      self.velocity[1] = 0
     
     #gravity with terminal v
     self.velocity[1] = min(5, self.velocity[1] + 0.1)
