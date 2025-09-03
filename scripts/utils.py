e8fc31ac9ffe92d3ed9d3e491433edaf0e0d06f4
captainCwangrz <18501397628@163.com>
2025-09-03 22:47:12 +0800

    added load_images() for loading entire folder of assets

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,8 +1,15 @@
-import pygame
+import pygame, os
 
 BASE_IMAGE_PATH = 'data/images/'
 
 def load_image(path, colorkey=(0, 0, 0)):
-  img = pygame.image.load(BASE_IMAGE_PATH+path).convert() #convert() makes img more efficient for pygame
+  img = pygame.image.load(BASE_IMAGE_PATH + path).convert() #convert() makes img more efficient for pygame
   img.set_colorkey(colorkey)
   return img
+
+def load_images(path, colorkey=(0, 0, 0)):
+  images = []
+  for img_name in sorted(os.listdir(BASE_IMAGE_PATH + path)): #sorted because tile type depends on this order
+    img = load_image(path + '/' + img_name)
+    images.append(img)
+  return images
