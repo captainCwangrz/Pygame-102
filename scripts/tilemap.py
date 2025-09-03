1644b1f6f5dbbdf2bcfcf7e4b151fe2ea7a7b831
captainCwangrz <18501397628@163.com>
2025-09-04 00:27:55 +0800

    added tiles_around() physics_tiles_around(). neighboring 9 tiles for now.

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,3 +1,9 @@
+import pygame
+
+TILE_OFFSETS = ((0,0),(0,1),(0,-1),(1,0),(1,1),(1,-1),(-1,0),(-1,1),(-1,-1))
+PHYSICS_TILES = {'Grass', 'Stone'}
+
+
 class Tilemap:
   def __init__(self, game, tile_size=16):
     self.game = game
@@ -9,6 +15,22 @@ class Tilemap:
       self.tilemap[str(3 + i) + ';10'] = {'type' : 'Grass', 'variant' : 1, 'pos' : (3 + i, 10)}
       self.tilemap['10;' + str(5 + i)] = {'type' : 'Stone', 'variant' : 1, 'pos' : (10, 5 + i)}
   
+  def tiles_around(self, pos):
+    tiles =[]
+    tile_loc = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size)) 
+    for offset in TILE_OFFSETS:
+      check_loc = str(tile_loc[0] + offset[0]) + ';' + str(tile_loc[1] + offset[1])
+      if check_loc in self.tilemap: #ignors empty space
+        tiles.append(self.tilemap[check_loc]) #adds the actual tile at that loc
+    return tiles
+
+  def physics_tiles_around(self, pos):
+    rects = []
+    for tile in self.tiles_around(pos):
+      if tile['type'] in PHYSICS_TILES:
+        rects.append(pygame.Rect(tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size, self.tile_size, self.tile_size))
+    return rects
+
   def render(self, surf):
     # Render offgrid stuff first because they are usually bg decors
     for tile in self.offgrid_tiles: 
@@ -17,6 +39,6 @@ class Tilemap:
     for tile in self.tilemap.values():
       loc = tile['pos']
       image = self.game.assets[tile['type']][tile['variant']]
-      surf.blit(image, (loc[0]*self.tile_size, loc[1]*self.tile_size))
+      surf.blit(image, (loc[0] * self.tile_size, loc[1] * self.tile_size))
       
   
