e34cd5284624ead9d1893af91dbe4365ae1de796
captainCwangrz <18501397628@163.com>
2025-09-04 23:53:00 +0800

    added camera movement (render everything with offset - self.scroll. Also added background

--- game.py
+++ game.py
@@ -16,17 +16,20 @@ class Game:
       'Decor' : load_images('tiles/decor'),
       'Grass' : load_images('tiles/grass'),
       'Large Decor' : load_images('tiles/large_decor'),
-      'Stone' : load_images('tiles/stone')
+      'Stone' : load_images('tiles/stone'),
+      'Background' : load_image('background.png')
     }
     self.player = PhysicsEntity(self, 'Player', (50, 50), (8, 15))
     self.movement = [False, False] #up, down
 
     self.tilemap = Tilemap(self)
 
+    self.scroll = [0, 0] 
+
   def run(self):
     while True:
       self.clock.tick(60)
-      self.display.fill((14, 219, 228))
+      self.display.blit(self.assets['Background'], (0, 0))
 
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -45,12 +48,15 @@ class Game:
           if event.key == pygame.K_RIGHT:
             self.movement[1] = False
 
+      self.scroll[0] += (self.player.rect().centerx - self.display.get_width() // 2 - self.scroll[0]) * 0.1
+      self.scroll[1] += (self.player.rect().centery - self.display.get_height() // 2 - self.scroll[1]) * 0.1
+      render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+      print(render_scroll)
       self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-      self.player.render(self.display)
-      self.tilemap.render(self.display)
+      self.player.render(self.display, offset = render_scroll)
+      self.tilemap.render(self.display, offset = render_scroll)
       self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
 
-      print(self.tilemap.physics_tiles_around(self.player.pos))
       pygame.display.flip() #safe full-page refresh, otherwise use update([rect_list])
 
 Game().run()
