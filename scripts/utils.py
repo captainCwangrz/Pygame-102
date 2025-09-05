6983faa9ba6793e5a67030455fc07a95bd215c26
captainCwangrz <18501397628@163.com>
2025-09-06 01:30:18 +0800

    added Animation class

--- scripts/utils.py
+++ scripts/utils.py
@@ -13,3 +13,25 @@ def load_images(path, colorkey=(0, 0, 0)):
     img = load_image(path + '/' + img_name)
     images.append(img)
   return images
+
+class Animation:
+  def __init__(self, images, frame_duration = 5, loop = True):
+    self.images = images
+    self.duration = frame_duration
+    self.loop = loop
+    self.frame_count = 0
+    self.done = False
+
+  def copy(self):
+    return Animation(self.images, self.duration, self.loop)
+
+  def update(self):
+    if self.loop:
+      self.frame_count = (self.frame_count + 1) % (self.duration * len(self.images))
+    else:
+      self.frame_count = min(self.frame_count + 1, self.duration * len(self.images) - 1)
+      if self.frame_count >= self.duration * len(self.images) - 1:
+        self.done = True
+
+  def get_current_image(self):
+    return self.images[self.frame_count // self.duration]
