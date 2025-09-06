e7bcac10704be07e4771f28e63c4e86a1e853897
captainCwangrz <18501397628@163.com>
2025-09-06 21:30:34 +0800

    Added load() and save() json from map editor

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, json
 
 TILE_OFFSETS = ((0,0),(0,1),(0,-1),(1,0),(1,1),(1,-1),(-1,0),(-1,1),(-1,-1))
 PHYSICS_TILES = {'Grass', 'Stone'}
@@ -10,10 +10,6 @@ class Tilemap:
     self.tile_size = tile_size
     self.tilemap = {}
     self.offgrid_tiles = []  #not perfectly aligned sutff?
-
-    for i in range(10):
-      self.tilemap[str(3 + i) + ';10'] = {'type' : 'Grass', 'variant' : 1, 'pos' : (3 + i, 10)}
-      self.tilemap['10;' + str(5 + i)] = {'type' : 'Stone', 'variant' : 1, 'pos' : (10, 5 + i)}
   
   def tiles_around(self, pos):
     tiles =[]
@@ -44,4 +40,15 @@ class Tilemap:
           image = self.game.assets[tile['type']][tile['variant']]
           surf.blit(image, (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))
       
-  
+  def save(self, path):
+    f = open(path, 'w')
+    json.dump({'tilemap' : self.tilemap, 'tile_size' : self.tile_size, 'offgrid' : self.offgrid_tiles}, f)
+    f.close()
+      
+  def load(self, path):
+    f = open(path, 'r')
+    map_data = json.load(f)
+    f.close()
+    self.tilemap = map_data['tilemap']
+    self.tile_size = map_data['tile_size']
+    self.offgrid_tiles = map_data['offgrid']
